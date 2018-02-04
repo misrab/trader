642986ee27f5408a8e646a5c48f5807736192bc6
Misrab <faizullah.misrab@gmail.com>
2018-02-04 17:23:39 +0700

    commit

--- scraper/kraken_test.go
+++ scraper/kraken_test.go
@@ -2,8 +2,18 @@ package scraper
 
 import (
 	"testing"
+
+	"fmt"
 )
 
 func TestKrakenTime(t *testing.T) {
 	println("Testing time...")
+
+	kraken := NewKraken("", "")
+	time, err := kraken.Time()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("Time is %v\n", time)
 }
